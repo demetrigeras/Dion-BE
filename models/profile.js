fd718fa80d8f5910acf76989ca7b7b29970d8346
demetrigeras <demetrijgeras@gmail.com>
2023-12-18 23:02:50 -0500

    models

---
+++ models/profile.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import User from './user.js';
+
+const profileSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
+  name:{type: String},
+  photo: {type: String},
+  bio: {type: String},
+  DOB: {type: Date},
+  createdEvents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }] 
+});
+
+export default mongoose.model('Profile', profileSchema);
